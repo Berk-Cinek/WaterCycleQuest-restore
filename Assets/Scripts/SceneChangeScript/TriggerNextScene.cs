252363476a3115e45b83dd84397197ba214e5ae7
Berk-Cinek <cinek.berkutkan@gmail.com>
2024-11-28 13:06:52 +0300

    Revert "Merge branch 'Front'"
    This reverts commit c62a1ed15b97bad1de316b985b3c21d861d553e6, reversing
    changes made to a96407214907da2769abe2d07bd9fa43abeebe7e.

--- Assets/Scripts/SceneChangeScript/TriggerNextScene.cs
+++ Assets/Scripts/SceneChangeScript/TriggerNextScene.cs
@@ -5,7 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class TriggerNextScene : MonoBehaviour
 {
-    [SerializeField] private bool requireKeyPress = true;                         // Tuþa basýlmasý gereksinimini inspector'e ekler
     private bool isPlayerNearby = false;
 
     private void OnTriggerEnter2D(Collider2D collider)
@@ -13,30 +12,14 @@ public class TriggerNextScene : MonoBehaviour
         if (collider.gameObject.CompareTag("Player"))
         {
             isPlayerNearby = true;
-
-            if (!requireKeyPress)                                                // Eðer tuþa basýlmasý gerekmiyorsa player'i collider içine girdiðinde sonraki scene ýþýnlar
-            {
-                LoadNextScene();
-            }
         }
     }
+
     private void Update()
     {
-        if (requireKeyPress && isPlayerNearby && Input.GetKeyDown(KeyCode.E))    // Eðer tuþa basýlmasý gerekiyorsa ve E tuþuna basýlýrsa player'i sonraki scene ýþýnlar
+        if (isPlayerNearby && Input.GetKeyDown(KeyCode.E))
         {
-            LoadNextScene();
-        }
-    }
-    private void LoadNextScene()
-    {
-        int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
-        if (nextSceneIndex < SceneManager.sceneCountInBuildSettings)
-        {
-            SceneManager.LoadScene(nextSceneIndex);
-        }
-        else
-        {
-            Debug.LogWarning("Son sahneye ulaþýldý");
+            SceneManager.LoadScene("RuledProceduralMap");
         }
     }
 }
