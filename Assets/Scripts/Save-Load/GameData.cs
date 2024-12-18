f21a269c6f52ee6809d57c5b9d1e33a145978416
kokmustafaemre <kok.mustafaemre@student.atilim.edu.tr>
2024-12-18 16:10:32 +0300

    village diolog added

---
+++ Assets/Scripts/Save-Load/GameData.cs
@@ -0,0 +1,18 @@
+[System.Serializable]
+public class GameData
+{
+    public int levelIndex;       // Aktif sahne numarasý
+    public float playerPosX;     // Oyuncunun X pozisyonu
+    public float playerPosY;     // Oyuncunun Y pozisyonu
+    public float playerHealth;   // Oyuncunun caný (varsa)
+    public float enemyPosX;
+    public float enemyPosY;
+    public float enemyHealth;
+    public int coin;
+    public int waveCount;
+    public int healthUpgradeCount;
+    public int damageUpgradeCount;
+    public int speedUpgradeCount;
+    public int dashSpeedUpgradeCount;
+    
+}
