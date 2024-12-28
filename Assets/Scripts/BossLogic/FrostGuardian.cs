a9b5ce8ebd7ec32160e75a51394af33aeb0402ed
kokmustafaemre <kok.mustafaemre@student.atilim.edu.tr>
2024-12-28 15:33:24 +0300

    frost guardian multible drop fixed

--- Assets/Scripts/BossLogic/FrostGuardian.cs
+++ Assets/Scripts/BossLogic/FrostGuardian.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 
-public class FrostGuardian : MonoBehaviour,IDamageable
+public class FrostGuardian : MonoBehaviour, IDamageable
 {
     Transform TargetPos;
     [SerializeField] private int health;
@@ -14,19 +14,22 @@ public class FrostGuardian : MonoBehaviour,IDamageable
     Animator animator;
     float Speed = 5f;
     public bool isRunning = true;
-    private float lastDamageTime =0f;
+    private float lastDamageTime = 0f;
     private float damageCooldown = 1f;
+    private Rigidbody2D rb;
     [SerializeField] private int damage = 20;
 
     [SerializeField] private GameObject instructorPrefab;
     [SerializeField] private GameObject healthItemPrefab;
     [SerializeField] private GameObject coinPrefab;
+    private bool isDead = false; // New flag to track death state
     public GameObject bulletPrefab;
     public event System.Action OnDeath;
 
     // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         bodySprite = GetComponent<SpriteRenderer>();
     }
@@ -34,6 +37,7 @@ public class FrostGuardian : MonoBehaviour,IDamageable
     // Update is called once per frame
     void Update()
     {
+        if (isDead) return; // Skip all behavior if dead
         GetTargetPos();
         SetSpriteFlip();
         GetDirection();
@@ -44,8 +48,10 @@ public class FrostGuardian : MonoBehaviour,IDamageable
     }
 
 
-    void GetTargetPos(){
-        if(GameObject.FindGameObjectWithTag("Player") !=null){
+    void GetTargetPos()
+    {
+        if (GameObject.FindGameObjectWithTag("Player") != null)
+        {
             TargetPos = GameObject.FindGameObjectWithTag("Player").transform;
         }
     }
@@ -57,11 +63,11 @@ public class FrostGuardian : MonoBehaviour,IDamageable
 
     void SetSpriteFlip()
     {
-        if(TargetPos.position.x - transform.position.x < 0)
+        if (TargetPos.position.x - transform.position.x < 0)
         {
             bodySprite.flipX = false;
         }
-        else if (TargetPos.position.x - transform.position.x >0)
+        else if (TargetPos.position.x - transform.position.x > 0)
         {
             bodySprite.flipX = true;
         }
@@ -70,7 +76,8 @@ public class FrostGuardian : MonoBehaviour,IDamageable
 
     void Attack()
     {
-        if (ray.collider != null && ray.collider.CompareTag("Player")){
+        if (ray.collider != null && ray.collider.CompareTag("Player"))
+        {
             isRunning = false;
             animator.SetTrigger("Attack");
         }
@@ -79,7 +86,7 @@ public class FrostGuardian : MonoBehaviour,IDamageable
             isRunning = true;
             animator.SetTrigger("run");
         }
-        
+
     }
 
     void Run()
@@ -93,8 +100,10 @@ public class FrostGuardian : MonoBehaviour,IDamageable
 
     }
 
-     public void Damage(int damageAmount)
+    public void Damage(int damageAmount)
     {
+        if (isDead) return; // Don't process damage if already dead
+
         health -= damageAmount;
         if (health <= 0)
         {
@@ -104,19 +113,28 @@ public class FrostGuardian : MonoBehaviour,IDamageable
 
     private void Die()
     {
-        animator.SetTrigger("death");
+        if (isDead) return; // Prevent duplicate calls
+        isDead = true;
+
         OnDeath?.Invoke();
+        animator.SetTrigger("death");
         DropCoin();
         DropHealthItem();
         DropInstructor();
+        GetComponent<Collider2D>().enabled = false; // Disable the collider
+        rb.velocity = Vector2.zero; // Stop any movement
+        rb.isKinematic = true; // Prevent physics interactions
     }
 
-    private void destroy(){
+    private void destroy()
+    {
         Destroy(gameObject);
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
+        if (isDead) return; // Ignore interactions if dead
+
         if (other.gameObject.CompareTag("Player"))
         {
             // only apply damage if enough time has passed since the last damage 
